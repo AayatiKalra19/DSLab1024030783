de972bdb2a5036c9fff0196ecf293c728dd6088a
AayatiKalra19 <kalraaayati@gmail.com>
2025-11-17 07:57:03 +0530

    Add files via upload

---
+++ Assignment-9/BFS.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+vector <int> BFS(int vertex, vector<int> adj[]) {
+    int vis[vertex] = {0};
+    vis[0] = 1;
+    queue <int> q;
+    vector <int> bfs;
+    q.push(0);
+    while(!q.empty()) {
+        int node = q.front();
+        q.pop();
+        bfs.push_back(node);
+
+        for (auto it: adj[node]) {
+            if (!vis[it]) {
+                vis[it] = 1;
+                q.push(it);
+            }
+        }
+    }
+    return bfs;
+}    
